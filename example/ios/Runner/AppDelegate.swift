b40cd1010cfb5273da71ae1e984753d1b121ea13
Daniil Alferov <dal@efficy.com>
2022-03-25 16:52:51 +0300

    Update iOS example application

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,41 @@
 import UIKit
 import Flutter
+import CoreLocation
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate {
+    
+    private var locationManager: CLLocationManager = CLLocationManager()
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    self.locationManager.delegate = self
+    self.requestLocationAuthorization()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func requestLocationAuthorization() {
+        switch self.locationStatus {
+        case .notDetermined:
+            self.locationManager.requestAlwaysAuthorization()
+        default:
+            break
+        }
+    }
+    
+    private var locationStatus: CLAuthorizationStatus {
+        var status: CLAuthorizationStatus = .notDetermined
+        if #available(iOS 14.0, *) {
+            status = self.locationManager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+        return status
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {}
+
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,17 +15,18 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class CounterIncrementor extends StatelessWidget {
-  const CounterIncrementor({required this.onPressed, Key? key})
+class CustomButton extends StatelessWidget {
+  const CustomButton({required this.onPressed, required this.title, Key? key})
       : super(key: key);
 
   final VoidCallback onPressed;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: const Text('Provide consent'),
+      child: Text(title),
     );
   }
 }
@@ -34,7 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    // ApsisOne.setMinimumLogLevel(ONELogLevel.info);
+    ApsisOne.setMinimumLogLevel(ONELogLevel.info);
   }
 
   @override
@@ -45,17 +46,97 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          // child: Text('ApsisOne App Tool Example App'),
-          child: CounterIncrementor(onPressed: _increment),
+          child: Column (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CustomButton (onPressed: _provideCollectDataConsent,  title: 'Provide data consent'),
+              CustomButton (onPressed: _removeCollectDataConsent,  title: 'Remove data consent'),
+              CustomButton (onPressed: _provideCollectLocationConsent,  title: 'Provide location consent'),
+              CustomButton (onPressed: _removeCollectLocationConsent,  title: 'Remove location consent'),
+              CustomButton (onPressed: _trackScreenViewEvent,  title: 'Track screenView event'),
+              CustomButton (onPressed: _trackCustomEvent,  title: 'Track custom event'),
+              CustomButton (onPressed: _trackCustomLocation,  title: 'Track custom location'),
+              CustomButton (onPressed: _startCollectLocationLow,  title: 'Start collecting location low'),
+              CustomButton (onPressed: _startCollectLocationMedium,  title: 'Start collecting location medium'),
+              CustomButton (onPressed: _startCollectLocationHigh,  title: 'Start collecting location high'),
+              CustomButton (onPressed: _stopCollectLocation,  title: 'Stop collecting location'),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _increment() {
+  void _provideCollectDataConsent() {
     setState(() {
-      ApsisOne.setMinimumLogLevel(ONELogLevel.warning);
-      // ApsisOne.provideConsent(ONEConsentType.collectData);
+      ApsisOne.provideConsent(ONEConsentType.collectData);
+    });
+  }
+
+  void _removeCollectDataConsent() {
+    setState(() {
+      ApsisOne.removeConsent(ONEConsentType.collectData);
+    });
+  }
+
+  void _provideCollectLocationConsent() {
+    setState(() {
+      ApsisOne.provideConsent(ONEConsentType.collectLocation);
+    });
+  }
+
+  void _removeCollectLocationConsent() {
+    setState(() {
+      ApsisOne.removeConsent(ONEConsentType.collectLocation);
+    });
+  }
+
+  void _trackScreenViewEvent() {
+    setState(() {
+      ApsisOne.trackScreenViewEvent('Custom view title');
+    });
+  }
+
+  void _trackCustomEvent() {
+    setState(() {
+      var testCustomEventData = {
+        'source' : 'testSource',
+        'number' : 321,
+        'numberWithDecimal' : 3.14159,
+        'text' : 'testText',
+        'trueFalse' : true
+      };
+      ApsisOne.trackCustomEvent('com.apsis1.events.transaction.custom-testcustomevent-3c3yitjo4j', testCustomEventData);
+    });
+  }
+
+  void _trackCustomLocation() {
+    setState(() {
+      ApsisOne.trackLocation(53.425, 38.543, 'Test placemark name', 'Test placemark address', 3);
+    });
+  }
+
+  void _startCollectLocationLow() {
+    setState(() {
+      ApsisOne.startCollectingLocation(ONELocationFrequency.low);
+    });
+  }
+
+  void _startCollectLocationMedium() {
+    setState(() {
+      ApsisOne.startCollectingLocation(ONELocationFrequency.medium);
+    });
+  }
+
+  void _startCollectLocationHigh() {
+    setState(() {
+      ApsisOne.startCollectingLocation(ONELocationFrequency.high);
+    });
+  }
+
+  void _stopCollectLocation() {
+    setState(() {
+      ApsisOne.stopCollectingLocation();
     });
   }
 }
