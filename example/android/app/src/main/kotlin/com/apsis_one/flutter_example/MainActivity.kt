a03d41bbfdf01762d525d0609585d175f85d9c36
Vasiliy Makarov <drmoriarty.0@gmail.com>
2022-04-04 16:16:52 +0300

    add method channel to main activity

--- example/android/app/src/main/kotlin/com/apsis_one/flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/apsis_one/flutter_example/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.apsis_one.flutter_example
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private lateinit var appChannel : MethodChannel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appChannel = MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, "com.apsis.one/sampleapp")
+        appChannel.setMethodCallHandler(MethodChannel.MethodCallHandler { call, result ->
+            if (call.method == "switchView") {
+                // run another activity
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        })
+
+    }
 }
